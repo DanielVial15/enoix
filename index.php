108722e9148a029cc2ad308d4065d97f6806c4bf
Daniel Carneiro dos Santos Vial - 11815876 <11815876@animaedu.intranet>
2019-10-04 20:34:41 -0300

    tamo junto

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+define("IP","127.0.0.1");
+echo IP;
+echo "<br>";
+$numero = 1;
+echo $numero;
+echo "<br>";
+echo "Olá Mundo";
+echo "<br>";
+$num1 = 1;
+$num2 = 2;
+echo("O resultado da soma é:".($num1+$num2));
